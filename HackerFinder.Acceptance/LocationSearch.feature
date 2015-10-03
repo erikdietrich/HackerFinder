e3c0dad23bdfbc1ca4fd7cbe463d30871e626a1e
Erik Dietrich <erik@daedtech.com>
2015-10-03 10:25:30 -0500

    Added first spec flow feature.  It's not especially useful just yet.

---
+++ HackerFinder.Acceptance/LocationSearch.feature
@@ -0,0 +1,6 @@
+﻿Feature: LocationSearch
+	
+
+Scenario: Chicago Search
+	When I supply location Chicago
+	Then I should have a user named Erik
---
+++ HackerFinder.Acceptance/LocationSearchSteps.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace HackerFinder.Acceptance
+{
+    [Binding]
+    public class LocationSearchSteps
+    {
+        [When(@"I supply location (.*)")]
+        public void WhenIPressAdd(string locationText)
+        {
+            
+        }
+        
+        [Then(@"I should have a user named (.*)")]
+        public void ThenTheResultShouldBeOnTheScreen(string userFirstName)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
