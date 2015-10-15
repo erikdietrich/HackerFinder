d6346cfc70f2277f33e80e1fc187092822c94fd1
Erik Dietrich <erik@daedtech.com>
2015-10-15 10:32:34 -0500

    Distinguished between acceptance tests and end to end tests to get away from continuous test running against Github.

--- HackerFinder.Acceptance/LocationSearchSteps.cs
+++ HackerFinder.Acceptance/LocationSearchSteps.cs
@@ -1,28 +1,38 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Linq;
-using System.Collections.Generic;
+﻿using System;
 using TechTalk.SpecFlow;
+using Telerik.JustMock;
+using System.Collections.Generic;
+using System.Linq;
+using Telerik.JustMock.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HackerFinder.Acceptance
 {
     [Binding]
     public class LocationSearchSteps
     {
+        private static ScenarioContext Current { get { return ScenarioContext.Current; } }
+
         [When(@"I supply location (.*)")]
-        public void WhenISupplyLocation(string locationText)
+        public void WhenISupplyLocationWheelingIL(string locationText)
         {
-            var searcher = new ProfileSearcher(new GithubInquistor());
-            var profiles = searcher.GetProfilesForLocation(locationText);
+            var mockInquisitor = Mock.Create<IGithubInquistor>();
+            mockInquisitor.Arrange(inquisitor => inquisitor.ExecuteUrlQuery(Arg.AnyString)).Returns("erikdietrich");
 
-            ScenarioContext.Current.Set(profiles);
+            var target = new ProfileSearcher(mockInquisitor);
+
+            var profiles = target.GetProfilesForLocation(locationText);
+
+            Current.Set(profiles);
         }
-        
-        [Then(@"I should have a user named (.*)")]
-        public void ThenIShouldHaveAUserNamed(string userFirstName)
-        {
-            var profiles = ScenarioContext.Current.Get<IList<Profile>>();
 
-            Assert.IsTrue(profiles.Any(pr => pr.FirstName == userFirstName));
+        [Then(@"I should have a user named (.*)")]
+        public void ThenIShouldHaveAUserNamedErik(string firstName)
+        {
+            var profiles = Current.Get<IList<Profile>>();
+
+            Assert.IsTrue(profiles.Any(p => p.FirstName == firstName));
+
         }
     }
 }
---
+++ HackerFinder.EndToEnd/LocationSearchSteps.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace HackerFinder.EndToEnd
+{
+    [Binding]
+    public class LocationSearchSteps
+    {
+        [When(@"I supply location (.*)")]
+        public void WhenISupplyLocation(string locationText)
+        {
+            var searcher = new ProfileSearcher(new GithubInquistor());
+            var profiles = searcher.GetProfilesForLocation(locationText);
+
+            ScenarioContext.Current.Set(profiles);
+        }
+        
+        [Then(@"I should have a user named (.*)")]
+        public void ThenIShouldHaveAUserNamed(string userFirstName)
+        {
+            var profiles = ScenarioContext.Current.Get<IList<Profile>>();
+
+            Assert.IsTrue(profiles.Any(pr => pr.FirstName == userFirstName));
+        }
+    }
+}
