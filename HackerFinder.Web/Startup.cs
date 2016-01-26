7cc59810a389bd1ab445a980d937adf5039bf90b
Erik Dietrich <erik@daedtech.com>
2016-01-26 11:15:24 -0600

    Added a web project that will (hopefully) make AppHarbor happy.

---
+++ HackerFinder.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HackerFinder.Web.Startup))]
+namespace HackerFinder.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
